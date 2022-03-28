aa318dc53251d712a06bd0d35aaeca9cdea144cc
Aishels <aishelweinberg@gmail.com>
2022-03-27 21:31:13 -0400

    added in time slot vars to js

---
+++ Develop/script.js
@@ -0,0 +1,35 @@
+// moment.js
+var currentDate = moment().format('dddd') + " " + moment().format("Do MMM YYYY");
+var currentHour = moment().format('h:mm:ss a');
+// hour var
+var nineAm = $("#9am");
+var tenAm = $("#10am");
+var elevenAm = $("#11am");
+var twelvePm = $("#12pm");
+var onePm = $("#13pm");
+var twoPm = $("#14pm");
+var threePm = $("#15pm");
+var fourPm = $("#16pm");
+var fivePm = $("#17pm");
+var sixPm = $("#18pm");
+var sevenPm = $("#19pm");
+// var hourString = $(".hour").text().split(" ");
+var hour = moment().hours();
+var userInput;
+var hourSpan;
+
+// date and hour
+
+var interval = setInterval(function() {
+    var momentNow = moment();
+    $('#currentDay').html(momentNow.format('YYYY MMMM DD') + ' '
+                        + momentNow.format('dddd')
+                         .substring(0,3).toUpperCase());
+    $('#currentDay').html(currentDate + " " + momentNow.format('hh:mm:ss A'));
+  }, 100);
+
+
+
+
+
+//   buttons and local storage
